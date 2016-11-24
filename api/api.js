cb759fb4edc3efd7593fe0a9292528130139b54c
giapvn <giapvn.fr@gmail.com>
2016-11-24 17:22:32 +0700

    add getnewproduct for mod

--- api/api.js
+++ api/api.js
@@ -4,11 +4,11 @@ var guestController = require("../controllers/guest_controller.js");
 var customerController = require('../controllers/customer_controller');
 var providerController = require('../controllers/provider_controller.js');
 var productController = require('../controllers/product_controller.js');
-var moderatorController=require('../controllers/moderator_controller.js');
+var moderatorController = require('../controllers/moderator_controller.js');
 
 //api guest
 
-router.post("/signup", guestController.sign_up); 
+router.post("/signup", guestController.sign_up);
 // router.get('/login/:username/:password', guestController.login);
 router.post('/login', guestController.login);
 router.get('/search/:keyword', guestController.search_product_by_name);
@@ -24,15 +24,17 @@ router.get('/viewprofile/:username');
 router.post('/review/:username/:product_id');
 router.get('/viewcart/:username');
 router.post('/editcart/')
-//api provider
+    //api provider
 router.post('/resign/:username');
-router.post('/addproduct',providerController.add_product);
-router.post('/editproduct',providerController.edit_product);
-router.post('/removeproduct',providerController.delete_product);
+router.post('/addproduct', providerController.add_product);
+router.post('/editproduct', providerController.edit_product);
+router.post('/removeproduct', providerController.delete_product);
 router.post('/confirm');
 //moderator
-router.post('/signin',moderatorController.signin);
-
+router.post('/signin', moderatorController.signin);
+// xem va xoa cac san pham khong hop le
+router.get('/getnewproduct/:category', moderatorController.get_new_product);
+// router.post('/deleteproduct', moderatorController.deleteProduct);
 //admin
 router.post('/addmod');
 router.post('/deletemod');
@@ -41,6 +43,6 @@ router.post('/editcategory');
 router.post('/deletecategory');
 
 //product
-router.get('/getallproduct',productController.get_all_product);
+router.get('/getallproduct', productController.get_all_product);
 
 module.exports = router;
--- models/moderator_management.js
+++ models/moderator_management.js
@@ -1,31 +1,54 @@
 var notification = {};
 
-var ModeratorManagement = function(connection){
-	this.connection=connection;
+var ModeratorManagement = function(connection) {
+    this.connection = connection;
 };
 
 module.exports = ModeratorManagement;
 
-ModeratorManagement.prototype.findManager = function(manager, callback){
-	var collection=this.connection.collection('manager');
+ModeratorManagement.prototype.findManager = function(manager, callback) {
+    var collection = this.connection.collection('manager');
 
-	var notifyFindSuccess=function(_manager){
-		if(_manager==null){
-			notification.message="Username or password is incorrect!";
-			return callback(false, notification);
-		}else{
-			notification.message="Success!"
-			return callback(false, notification);	
-		}
-		
-	};
+    var notifyFindSuccess = function(_manager) {
+        if (_manager == null) {
+            notification.message = "Username or password is incorrect!";
+            return callback(false, notification);
+        } else {
+            notification.message = "Success!"
+            return callback(false, notification);
+        }
 
-	var notifyFindFail=function(err){
-		notification.error=err;
-		return callback(true, notification);
-	};
+    };
 
-	collection.findOne({username: manager.username, password: manager.password})
-	.then(notifyFindSuccess)
-	.catch(notifyFindFail);
+    var notifyFindFail = function(err) {
+        notification.error = err;
+        return callback(true, notification);
+    };
+
+    collection.findOne({ username: manager.username, password: manager.password })
+        .then(notifyFindSuccess)
+        .catch(notifyFindFail);
+};
+
+// Xem va xoa cac san pham khong hop le
+ModeratorManagement.prototype.getNewProduct = function(cate, callback) {
+    var collection = this.connection.collection('product');
+
+    var notifyFindSuccess = function(products) {
+        if (products == "" || products == null) {
+            notification.message = "No product found!";
+            return callback(false, notification);
+        }
+        return callback(false, notification);
+    };
+
+    var notifyFindFail = function(err) {
+        notification.error = err;
+        notification.message = "ERROR";
+        return callback(true, notification);
+    };
+
+    collection.find({ category: cate, checked: false }).toArray()
+        .then(notifyFindSuccess)
+        .catch(notifyFindFail);
 };
