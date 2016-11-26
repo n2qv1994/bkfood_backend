92a096b234e2fb0b275a7cc41b42cc440e6166c5
giapvn <giapvn.fr@gmail.com>
2016-11-26 07:49:16 +0700

    add api for delete moderator

--- models/admin_management.js
+++ models/admin_management.js
@@ -40,3 +40,40 @@ AdminManagement.prototype.createModerator = function(manager, callback) {
         .then(processSearchResult)
         .catch(notifyFindFail);
 };
+
+AdminManagement.prototype.deleteModerator = function(request, callback) {
+    var collection = this.connection.collection('manager');
+
+    var notifyFindFail = function(err) {
+        notification.message = "ERROR";
+        notification.error = err;
+        return callback(true, notification);
+    };
+
+    var verifyPassword = function(_admin) {
+        var notifyFail=function(err){
+            notification.message = "ERROR";
+            notification.error = err;
+            return callback(true, notification);
+        };
+
+        var deleteAccount=function(_moderator){
+            notification.message="Account is deleted";
+            return callback(false, notification);
+        };
+
+        if (_admin.password == request.password) {
+            collection.remove({ username: request.username })
+                .then(deleteAccount)
+                .catch(notifyFail);
+        }
+        else{
+            notification.message = "Password is incorrect!";
+            return callback(false, notification);
+        }
+    };
+
+    collection.findOne({ username: "admin" })
+        .then(verifyPassword)
+        .catch(notifyFindFail);
+};
